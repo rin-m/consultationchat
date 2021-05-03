8ad3688789e8d748cccd7704d4057aae6050463c
rin-m <s2010048@klis.tsukuba.ac.jp>
2021-05-03 20:30:39 +0900

    メッセージをデータベースに保存して送信

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -5,8 +5,18 @@ class RoomChannel < ApplicationCable::Channel
   def unsubscribed
   end
   def speak(data)
+    message = Message,create!(content: data["message"])
     ActionCable.server.broadcast(
       "room_channel", { message: data["message"] }
+    )
+  end
+
+  private
+
+  def render_message(message)
+    ApplicationController.render(
+      pratial: "message/message",
+      locals: { message: message }
     )
   end
 end
