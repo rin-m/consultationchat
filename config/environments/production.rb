cfc4d517f0b99bda88c2e1d7f63bbcda343ef698
rin-m <s2010048@klis.tsukuba.ac.jp>
2021-05-04 12:25:26 +0900

    [update]localhostでの接続に修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,9 +41,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = 'wss://example.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://localhost:3000', /https?:\/\/localhost:\d+/ ]
   config.action_cable.worker_pool_size = 10
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
